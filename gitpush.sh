7951463c9ff576f5fe7808e70f0d09d1117c8cdb
lqwangxg <lqwangxg@gmail.com>
2020-12-02 13:16:22 +0900

    first commit

---
+++ gitpush.sh
@@ -0,0 +1,20 @@
+git config --global user.email "lqwangxg@gmail.com"
+git config --global user.name "lqwangxg"
+
+## push an existing repository from the command line  
+#git remote add origin https://github.com/lqwangxg/mybot.git
+#git branch -M main
+#git push -u origin main
+
+## create a new repository on the command line
+#echo "# mybot" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/lqwangxg/mybot.git
+#git push -u origin main
+
+git add . 
+git commit -a -m "commit"
+git push -u origin master 
